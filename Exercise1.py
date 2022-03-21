9793a4d7580ed29afa2c6a88aca18f8a877330bc
nepo <tiago.nepo@gmail.com>
2022-03-21 12:18:58 +0000

    First commit

---
+++ Exercise1.py
@@ -0,0 +1,9 @@
+'''
+# Exercise: Print a nice greeting
+
+1. Ask the user (using input) to enter their name.
+2. Print a nice greeting to the user, using their name.
+'''
+
+name = input('Enter your name: ')
+print('Hello, ' + name + '!')
